496cf90500a318f952a84b0f9a8f080ff7e38012
mlarrad <marlarrad@gmail.com>
2023-03-29 14:03:24 +0200

    Note Calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+function notaFinal(bien1, mal1, bien2, mal2) {
+  let resultado = (bien1 - (mal1/3)) + ((bien2 * 4) - (mal2 * 1.33));
+  return resultado;
+}
+// console.log(notaFinal(26, 3, 2, 1));
+function solicitarDatos() {
+  let bien1 = parseInt(prompt("Ingrese la cantidad de respuestas correctas en la primera parte:"));
+  let mal1 = parseInt(prompt("Ingrese la cantidad de respuestas incorrectas en la primera parte:"));
+  let bien2 = parseInt(prompt("Ingrese la cantidad de respuestas correctas en la segunda parte:"));
+  let mal2 = parseInt(prompt("Ingrese la cantidad de respuestas incorrectas en la segunda parte:"));
+
+  return { bien1, mal1, bien2, mal2 };
+}
+
+let datos = solicitarDatos();
+console.log(notaFinal(datos.bien1, datos.mal1, datos.bien2, datos.mal2));
+
+let parrafo = document.getElementById('resultado');
+parrafo.innerHTML = notaFinal(datos.bien1, datos.mal1, datos.bien2, datos.mal2);
